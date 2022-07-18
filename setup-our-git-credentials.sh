dc14d5757de441c4a204218e4b40efd88b208812
arcolinuxz <arcolinuxinfo@gmail.com>
2022-07-18 20:59:22 +0200

    test push

--- setup-our-git-credentials.sh
+++ setup-our-git-credentials.sh
@@ -1,24 +1,38 @@
 #!/bin/bash
-#
-#tput setaf 0 = black
-#tput setaf 1 = red
-#tput setaf 2 = green
-#tput setaf 3 = yellow
-#tput setaf 4 = dark blue
-#tput setaf 5 = purple
-#tput setaf 6 = cyan
-#tput setaf 7 = gray
-#tput setaf 8 = light blue
+#set -e
 ##################################################################################################################
-# Written to be used on 64 bits computers
-# Author 	: 	Erik Dubois
-# Website 	: 	http://www.erikdubois.be
-##################################################################################################################
+# Author 	: Erik Dubois
+# Website   : https://www.erikdubois.be
+# Website   : https://www.alci.online
+# Website	: https://www.arcolinux.info
+# Website	: https://www.arcolinux.com
+# Website	: https://www.arcolinuxd.com
+# Website	: https://www.arcolinuxb.com
+# Website	: https://www.arcolinuxiso.com
+# Website	: https://www.arcolinuxforum.com
 ##################################################################################################################
 #
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #
 ##################################################################################################################
+#tput setaf 0 = black 
+#tput setaf 1 = red 
+#tput setaf 2 = green
+#tput setaf 3 = yellow 
+#tput setaf 4 = dark blue 
+#tput setaf 5 = purple
+#tput setaf 6 = cyan 
+#tput setaf 7 = gray 
+#tput setaf 8 = light blue
+##################################################################################################################
+
+echo
+tput setaf 3
+echo "################################################################"
+echo "################### Start"
+echo "################################################################"
+tput sgr0
+echo
 
 # Problem solving commands
 
@@ -45,70 +59,17 @@ echo "--------------------------------------------------------------------------
 echo "this is project https://github.com/$githubdir/$project"
 echo "-----------------------------------------------------------------------------"
 
+git config --global pull.rebase false
+git config --global push.default simple
+git config --global user.name "arcolinuxz"
+git config --global user.email "arcolinuxinfo@gmail.com"
+sudo git config --system core.editor nano
+git remote set-url origin git@github.com-arc:$githubdir/$project
+
 echo
-tput setaf 1
+tput setaf 3
 echo "################################################################"
-echo "#####  Choose wisely - one time setup after clean install   ####"
+echo "################### End"
 echo "################################################################"
 tput sgr0
 echo
-echo "Select the correct desktop"
-echo
-echo "0.  Do nothing"
-echo "1.  Erik"
-echo "2.  Raniel"
-echo "3.  Steve"
-echo "Type the number..."
-
-read CHOICE
-
-case $CHOICE in
-
-    0 )
-      echo
-      echo "########################################"
-      echo "We did nothing as per your request"
-      echo "########################################"
-      echo
-      ;;
-
-    1 )
-			git config --global pull.rebase false
-			git config --global push.default simple
-			git config --global user.name "arcolinuxz"
-			git config --global user.email "arcolinuxinfo@gmail.com"
-			sudo git config --system core.editor nano
-			#git config --global credential.helper cache
-			#git config --global credential.helper 'cache --timeout=32000'
-      git remote set-url origin git@github.com-arc:$githubdir/$project
-      echo
-      echo "Everything set"
-      ;;
-    2 )
-			git config --global pull.rebase false
-			git config --global push.default simple
-			git config --global user.name "Raniel Laguna"
-			git config --global user.email "avraniel@gmail.com"
-			sudo git config --system core.editor nano
-			git config --global credential.helper cache
-			git config --global credential.helper 'cache --timeout=32000'
-      ;;
-    3 )
-			git config --global pull.rebase false
-			git config --global push.default simple
-			git config --global user.name "Steve Younger"
-			git config --global user.email "coritanie@gmail.com"
-			sudo git config --system core.editor nano
-			git config --global credential.helper cache
-			git config --global credential.helper 'cache --timeout=32000'
-      ;;
-    * )
-      echo "#################################"
-      echo "Choose the correct number"
-      echo "#################################"
-      ;;
-esac
-
-echo "################################################################"
-echo "###################    T H E   E N D      ######################"
-echo "################################################################"
